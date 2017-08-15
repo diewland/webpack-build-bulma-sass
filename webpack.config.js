6746f109abea53a6b000f3576bbe95793b5d4dac
Yuwat Chuesathuchon <yuwat@mfec.co.th>
2017-08-15 16:00:47 +0700

    add initialize files

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.sass$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader', options: { minimize: true } },
+            { loader: 'sass-loader' },
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('bulma.min.css')
+  ]
+}
